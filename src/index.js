418d78b4b9785ab0b63c08c20c4caade30e1f06d
njrapidinnovation <nj@rapidinnovation.dev>
2023-07-14 18:23:59 +0530

    vercel web analytics added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { inject } from '@vercel/analytics';
+
+inject()
 
 ReactDOM.render(
   <React.StrictMode>
